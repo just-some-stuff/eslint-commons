6538a3a0d56bb0ac33bbf2f1d7e39aecbe3a8ad1
Arpit Jain <arpitjp@gmail.com>
2022-07-16 17:42:35 +0530

    init

--- index.js
+++ index.js
@@ -6,8 +6,31 @@ module.exports = {
     'eslint:recommended',
     'plugin:node/recommended-module',
     'plugin:import/recommended',
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  plugins: ['jest', 'import'],
+  plugins: ['import'],
+  overrides: [
+    {
+      // jest only on test files
+      "files": [
+        "**/*.test.js",
+        "**/*.test.jsx",
+        "**/*.test.ts",
+        "**/*.test.tsx",
+      ],
+      "plugins": ["jest"],
+      "extends": ["plugin:jest/recommended"],
+      "rules": {
+        "jest/prefer-expect-assertions": "off",
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "warn",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "warn",
+      }
+    }
+  ],
   env: {
     node: true,
     'jest/globals': true,
@@ -19,14 +42,37 @@ module.exports = {
     __dirname: true
   },
   rules: {
-    'import/extensions': 'off',
+    'no-unused-vars': 'warn',
     'no-plusplus': 'off',
     'node/no-missing-import': 'off',
     'node/no-unpublished-import': 'off',
     'node/no-unpublished-require': 'off',
-    'node/no-process-exit': 'error',
+    'import/extensions': 'off',
     'import/no-unresolved': ['error', { commonjs: true }],
-    'import/order': 'error',
-    'import/first': 'error'
+    'import/order': [
+      'error',
+      {
+        pathGroups: [
+          {
+            pattern: '@*',
+            group: 'external',
+            position: 'after',
+          },
+          {
+            pattern: '@*/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
+    'import/first': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-key': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/jsx-uses-vars': 'error'
   }
 };
